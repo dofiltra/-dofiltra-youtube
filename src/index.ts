033838770b4093a1c7fa26c40faa59220e28016b
Dmitrii Osipovich <menemty@gmail.com>
2023-01-30 16:47:44 +0300

    v1.1.8

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { ProxyItem } from 'dprx-types';
 import { searchVideo } from './lib/search';
 
-export async function search({ searchQuery, proxy }: { searchQuery: string; proxy?: ProxyItem }) {
-  return await searchVideo({ searchQuery, proxy });
+export function search({ searchQuery, proxy }: { searchQuery: string; proxy?: ProxyItem }) {
+  return searchVideo({ searchQuery, proxy });
 }
