9e4af8cc05c13fbcc8bfa7be57457a5e4e05c0f4
Dmitrii Osipovich <menemty@gmail.com>
2024-07-20 14:14:27 +0300

    v1.1.22

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import { ProxyItem } from '@dofiltra/helpers';
 import { searchVideo } from './lib/search';
 
-export function search({ searchQuery, proxy }: { searchQuery: string; proxy?: ProxyItem }) {
-  return searchVideo({ searchQuery, proxy });
-}
+export const searchVideos = searchVideo;
